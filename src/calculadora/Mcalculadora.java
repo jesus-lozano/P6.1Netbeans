26706a9e3c3752112dffb4de6efa18168d9d0301
Jesus <Jesuslozarome@algo.com>
2021-05-04 20:22:07 +0200

    todas las opciones

--- src/calculadora/Mcalculadora.java
+++ src/calculadora/Mcalculadora.java
@@ -24,8 +24,8 @@ public class Mcalculadora {
         int operacion = 0;
         do {
             System.out.println(" Introduce Operacion a realizar");
-            /*System.out.println(" 1. suma \n 2. resta \n 3. multiplicacion \n 4. division \n 5. salir");*/
-            System.out.println(" 1. suma \n 2. resta \n 3. multiplicacion \n 4. salir");
+            System.out.println(" 1. suma \n 2. resta \n 3. multiplicacion \n 4. division \n 5. salir");
+          
             // leo por teclado un entero como opción del menú
             operacion = sc.nextInt();
             // segun valor elegido ejecuto método del objeto claculadora
@@ -39,17 +39,17 @@ public class Mcalculadora {
                case 3:
                     System.out.println(" la multipilcacion es " + casio.multiplicacion());
                     break;
-                    /* 
+                    
                 case 4:
                     System.out.println(" La division es " + casio.division());
                     break;
-            */
+            
                 default:
-                    System.out.println(" Debes elegir una opción de 1 a 3");
+                    System.out.println(" Debes elegir una opción de 1 a 5");
             }
 
 
-        } while (operacion != 4);
+        } while (operacion != 5);
 
         System.out.println(" Gracias por usar la calculadora...hasta luegooooorrr");
 
